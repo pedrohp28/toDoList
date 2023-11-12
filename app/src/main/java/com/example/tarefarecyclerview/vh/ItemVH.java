5ed20ddcf06d4db70d22f491be275fb1a2b658cf
Pedro Henrique Pereira <91918544+pedrohp28@users.noreply.github.com>
2023-11-12 17:45:22 -0300

    Update ItemVH.java

--- app/src/main/java/com/example/tarefarecyclerview/vh/ItemVH.java
+++ app/src/main/java/com/example/tarefarecyclerview/vh/ItemVH.java
@@ -2,33 +2,20 @@ package com.example.tarefarecyclerview.vh;
 
 import android.view.View;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.tarefarecyclerview.R;
 import com.example.tarefarecyclerview.model.ItemModel;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class ItemVH extends RecyclerView.ViewHolder{
 
     public TextView nomeTextView;
     public TextView descricaoTextView;
-    public TextView key;
-    FirebaseDatabase database;
-    DatabaseReference userRef;
-    String usuarioId;
     public ItemVH(@NonNull View itemView) {
         super(itemView);
 
         nomeTextView = itemView.findViewById(R.id.nomeTextView);
         descricaoTextView = itemView.findViewById(R.id.descricaoTextView);
         key = itemView.findViewById(R.id.txtKey);
-
-//        ItemModel item =new ItemModel();
-//        item.setKey(key.getText().toString());
-//        item.setNome(nomeTextView.getText().toString());
-//        item.setDescricao(descricaoTextView.getText().toString());
     }
 }
